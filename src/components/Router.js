a92e8fdd52595f98821647dc80248e7437120547
Choharim <jhl8586@naver.com>
2020-12-09 21:42:18 +0900

    Add log out

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,16 +2,22 @@ import React from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from 'routes/Home';
 import Auth from 'routes/Auth';
+import Profile from 'routes/Profile';
+import Navigation from 'components/Navigation';
 
 const AppRouter = ({isLoggedIn}) => {
 
   return (
     <Router>
+      {isLoggedIn && <Navigation></Navigation>}
       <Switch>
         {isLoggedIn ? 
         <>
         <Route exact path="/">
           <Home></Home>
+        </Route> 
+        <Route exact path="/profile">
+          <Profile></Profile>
         </Route> 
         </>: 
         <Route exact path="/">
