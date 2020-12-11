e8ac4ebee6f567e99684e1664a8b39834dbb2c53
Choharim <jhl8586@naver.com>
2020-12-11 13:13:21 +0900

    AttachmentImageFile

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from 'react';
 const Home = ({ userObj }) => {
   const [ nweet, setNweet ] = useState("");
   const [ nweets, setNweets ] = useState([]);
+  const [ attachment, setAttachment ] = useState(); 
 
   useEffect(() => {  
     dbService.collection("nweets").onSnapshot(snapshot => {
@@ -31,11 +32,26 @@ const Home = ({ userObj }) => {
     setNweet(value);
   };
 
+  const onFileChange = (event) => {
+    const { target: { files } } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = (finishedEvent) => { 
+      const { currentTarget: { result } } = finishedEvent;
+      setAttachment(result);
+     };
+
+    reader.readAsDataURL(theFile);
+  };
+
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input onChange={onChange} value={nweet} type="text" placeholder="What's on your mind?" maxLength={120}></input>
+        <input onChange={onFileChange} type="file" accept="image/*"></input>
         <input type="submit" value="Nweet"></input>
+        { attachment && <input type="image" src={attachment}></input> }
       </form>
       <div>
       {nweets.map((nweet) => (
